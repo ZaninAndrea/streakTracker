ae822942898d142149532c581550a55305e63112
Zanin Andrea <99.zanin@gmail.com>
2018-01-03 23:12:31 +0100

    can login to access counter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import React, {Component} from "react"
 import "./App.css"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import RaisedButton from "material-ui/RaisedButton"
 import Counter from "./Counter"
+
 class App extends Component {
     constructor() {
         super()
-        this.state = {count: 0}
-    }
-
-    async componentDidMount() {
-        const res = await fetch(
-            "https://count1up.herokuapp.com/total/exampleCounter?secret=36f721d4ff4b9cedd9ec866eb2c19dd5"
-        )
-        const resText = await res.text()
-        const count = parseFloat(resText)
-        console.log(resText)
-        this.setState({
-            count,
-        })
+        this.state = {count: 0, secret: "", counterName: ""}
     }
 
     render() {
         return (
             <MuiThemeProvider>
                 <div className="App">
-                    <div className="header">Streak tracker</div>
+                    <div className="header">
+                        <RaisedButton
+                            label="Login"
+                            className="loginButton"
+                            onClick={async () => {
+                                const secret = prompt("password")
+                                const counterName = prompt("counterName")
+
+                                this.setState({
+                                    secret,
+                                    counterName,
+                                })
+
+                                const res = await fetch(
+                                    `https://count1up.herokuapp.com/total/${counterName}?secret=${secret}`
+                                )
+                                const resText = await res.text()
+                                const count = parseFloat(resText)
+                                this.setState({
+                                    count,
+                                })
+                            }}
+                        />
+
+                        <span className="headerTitle">Streak tracker</span>
+                    </div>
                     <div className="main">
                         <Counter
                             count={this.state.count}
                             title="Example"
-                            counterName="exampleCounter"
-                            secret="36f721d4ff4b9cedd9ec866eb2c19dd5"
+                            counterName={this.state.counterName}
+                            secret={this.state.secret}
                             oneUp={() => {
                                 this.setState(({count}) => ({count: count + 1}))
                             }}
